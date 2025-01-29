2bedd17b84a399db43639e9bb9c555d5979103c6
n0urs <20nourabdulnasser00@gmail.com>
2025-01-29 20:44:31 +0200

    feat: lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  // popular-items tryout
+  { 
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'home', 
+    loadComponent: () => import('./components/home/home.component')
+      .then(m => m.HomeComponent)
+  }
 ];
