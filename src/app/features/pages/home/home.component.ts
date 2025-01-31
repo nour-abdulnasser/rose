db95154698635c40e44ec374fbdfb4cc3d5db55b
muhammed <muhammedabdelftah11@gmail.com>
2025-01-31 21:22:39 +0200

    about-us feature done

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PopularItemsComponent } from './components/popular-items/popular-items.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [PopularItemsComponent],
+  imports: [PopularItemsComponent , AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
