c8d6fd56595be2fe8cf46ba9d619cee8fcfca941
n0urs <20nourabdulnasser00@gmail.com>
2025-02-01 01:18:15 +0200

    feat: gallery images modified

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PopularItemsComponent } from './components/popular-items/popular-items.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-home',
-  imports: [PopularItemsComponent],
+  imports: [PopularItemsComponent, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
