0212c55e68e5407a788a25681e957cbf3b763d52
n0urs <20nourabdulnasser00@gmail.com>
2025-02-01 15:54:32 +0200

    feat: looping through gallery items rather than statically adding them in html

--- src/app/features/pages/home/components/gallery/gallery.component.ts
+++ src/app/features/pages/home/components/gallery/gallery.component.ts
@@ -4,8 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-gallery',
   imports: [],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  styleUrl: './gallery.component.scss',
 })
 export class GalleryComponent {
-
+  galleryImages = [
+    {
+      src: '/images/gallery-1.png',
+      colSpanClass: 'col-span-1',
+    },
+    {
+      src: '/images/gallery-2.png',
+      colSpanClass: 'col-span-1',
+    },
+    {
+      src: '/images/gallery-3.png',
+      colSpanClass: 'col-span-1',
+    },
+    {
+      src: '/images/gallery-4.png',
+      colSpanClass: 'col-span-1  md:col-span-2',
+    },
+    {
+      src: '/images/gallery-5.png',
+      colSpanClass: 'col-span-1',
+    },
+  ];
 }
