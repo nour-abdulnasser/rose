de309a7ca14fd7ee8439c54ef288016fcb234321
muhammed <muhammedabdelftah11@gmail.com>
2025-02-01 03:54:00 +0200

    trusted-by done

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PopularItemsComponent } from './components/popular-items/popular-items.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TrustedByComponent } from './components/trusted-by/trusted-by.component';
 
 @Component({
   selector: 'app-home',
-  imports: [PopularItemsComponent , AboutUsComponent],
+  imports: [PopularItemsComponent , AboutUsComponent , TrustedByComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
