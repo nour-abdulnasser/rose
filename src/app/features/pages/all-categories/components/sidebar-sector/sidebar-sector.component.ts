8a6ffc3a76d472cf957d0d0dc82900d32989720b
n0urs <20nourabdulnasser00@gmail.com>
2025-02-08 12:37:50 +0200

    feat: price range with binding

--- src/app/features/pages/all-categories/components/sidebar-sector/sidebar-sector.component.ts
+++ src/app/features/pages/all-categories/components/sidebar-sector/sidebar-sector.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { input } from '@angular/core';
 import { SidebarFilterContent } from '../../interfaces/sidebar-filter-content';
-import { TitleCasePipe } from '@angular/common';
+import { CurrencyPipe, TitleCasePipe } from '@angular/common';
 import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar-sector',
-  imports: [TitleCasePipe, MatSliderModule],
+  imports: [TitleCasePipe, MatSliderModule, FormsModule, CurrencyPipe],
   templateUrl: './sidebar-sector.component.html',
   styleUrl: './sidebar-sector.component.scss',
 })
 export class SidebarSectorComponent {
   sectorContent = input<SidebarFilterContent>();
+  selectedMinPrice = signal(0);
+  selectedMaxPrice = signal(80);
 }
